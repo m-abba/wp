d878f766d226a7935fc38872e81ef1bf880b8100
m-abba <meray.abba@rmit.edu.au>
2024-08-13 10:08:40 +1000

    Week 5 - 1 to 4 examples

---
+++ week05/Examples/script.js
@@ -0,0 +1,12 @@
+let a = parseInt(10.34); // a = 10
+let b = 20; 
+
+let sum = a + b; 
+let sub = a - b;
+let product = a * b;
+let div = a / b;
+
+console.log(" a + b = " + sum); 
+console.log(" a - b =  " + sub); 
+console.log(" a * b = " + product); 
+console.log(" a / b =  " + div); 
