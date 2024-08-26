43407a2c4b201663fbe26249b71704144770b3d2
m-abba <meray.abba@rmit.edu.au>
2024-08-27 09:48:35 +1000

    week 7 files added

---
+++ week07/example.php
@@ -0,0 +1,31 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+
+echo '<head>';
+echo '    <title>Example</title>';
+echo '</head>';
+
+echo '<body>';
+
+echo '<h1>Example</h1>';
+echo '<p> Hello World</p>';
+
+echo '</body>';
+
+echo '</html>';
+
+$var = "Meray";
+$VAR = 'Abba';
+
+
+echo "<p>" . $var . " " . $VAR . "</P>";
+// single line comment 
+
+/* 
+Mulitple line comment 
+echo '$var, $VAR, $var3';
+
+echo " He said 'Hello World'";
+echo "I'm a teacher";
+*/
