65df6f16acd6aaf667095dc06cac97d53055d38a
m-abba <meray.abba@rmit.edu.au>
2024-10-09 08:44:05 +1100

    logun

--- Week12/week12.1/Property/login_process.php
+++ Week12/week12.1/Property/login_process.php
@@ -1,23 +1,24 @@
 <?php
-$title = "Login Process";
-include('includes/db_connect.inc');
+session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$sql = "select username, password from users where username = ? and password = SHA(?)";
+include("includes/db_connect.inc");
+
+$sql = "select * from users where username = ? and password = SHA(?)";
 
 $stmt = $conn->prepare($sql);
+
 $stmt->bind_param("ss", $username, $password);
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 $stmt->execute();
 
 $result = $stmt->get_result();
-session_start();
+
+
 if ($result->num_rows > 0) {
     $_SESSION['username'] = $username;
-    // print ($_SESSION['username']);
 }
-
 $conn->close();
-header("Location: index.php"); // redirect to the index page
+header("Location:index.php");
 exit(0);
